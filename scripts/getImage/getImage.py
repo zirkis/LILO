5e51715e9cde48353ff7fd93414210b16d44ab7c
didelotK <Didelot_k@esme.fr>
2016-10-24 12:19:15 +0200

    Improve getImage scripts

--- scripts/getImage/getImage.py
+++ scripts/getImage/getImage.py
@@ -4,26 +4,36 @@ from icrawler.examples import GoogleImageCrawler
 from icrawler.examples import BingImageCrawler
 from icrawler.examples import BaiduImageCrawler
 
-print("Initialisation du script\n")
 
-maRecherche = "barbara palvin"
-nombreImage = 150
+def getImages(pathToSave, mySearch, numberOfPictures):
 
+	google_crawler = GoogleImageCrawler(path_to_save=pathToSave, img_dir=mySearch)
+	google_crawler.crawl(keyword=mySearch, offset=0, max_num=numberOfPictures,
+		date_min=None, date_max=None, feeder_thr_num=1,
+		parser_thr_num=1, downloader_thr_num=4,
+		min_size=(200,200), max_size=None)
+	"""
+	print("Bing\n")
+	pathToSave = "../../images/plants/Bing/"
+	bing_crawler = BingImageCrawler(path_to_save=pathToSave, img_dir=maRecherche)
+	bing_crawler.crawl(keyword=maRecherche, offset=0, max_num=nombreImage,
+	  feeder_thr_num=1, parser_thr_num=1,
+	  downloader_thr_num=4,
+		min_size=(200,200), max_size=None)
+	"""
 
-print("Google\n")
-pathToSave = "../../images/plants/Google/"
-google_crawler = GoogleImageCrawler(path_to_save=pathToSave, img_dir=maRecherche)
-google_crawler.crawl(keyword=maRecherche, offset=0, max_num=nombreImage,
-	date_min=None, date_max=None, feeder_thr_num=1,
-	parser_thr_num=1, downloader_thr_num=4,
-	min_size=(200,200), max_size=None)
+if __name__ == '__main__':
+	print("Initialisation du script\n")
 
-print("Bing\n")
-pathToSave = "../../images/plants/Bing/"
-bing_crawler = BingImageCrawler(path_to_save=pathToSave, img_dir=maRecherche)
-bing_crawler.crawl(keyword=maRecherche, offset=0, max_num=nombreImage,
-  feeder_thr_num=1, parser_thr_num=1,
-  downloader_thr_num=4,
-	min_size=(200,200), max_size=None)
+	numberOfPictures = 30
+	pathToSave = "../../images/plants/"
+	speciesSearch = ["Basil leaf",
+									 "Parsley leaf",
+									 "Mint leaf",
+									 "Thyme leaf",
+									 "Thai basil leaf",
+									 "Purple basil leaf"]
+	for specie in speciesSearch:
+		getImages(pathToSave, specie, numberOfPictures)						 
 
-print("Fin du script")
+	print("Fin du script\n")
