92e9f79163d17b1fe1c98c79afdc5c7ae9dbcfb7
didelotK <didelotKev@gmail.com>
2016-10-29 12:01:12 +0200

    Remove Scripts folder

--- Scripts/Pret a pousser/icrawler/examples/__init__.py
+++
@@ -1,7 +0,0 @@
-from .google import GoogleImageCrawler
-from .bing import BingImageCrawler
-from .baidu import BaiduImageCrawler
-from .flickr import FlickrImageCrawler
-from .greedy import GreedyImageCrawler
-
-__all__ = ['google', 'bing', 'baidu', 'flickr', 'greedy']
