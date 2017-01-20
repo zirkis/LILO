d237be64d27f4e598eabfb10761e3a8b0375d9a3
didelotK <didelotKev@gmail.com>
2017-01-20 17:32:57 +0100

    correct ansible

--- app/deep_learning/examples/extract_frame_from_video.py
+++ app/deep_learning/examples/extract_frame_from_video.py
@@ -6,10 +6,10 @@ if __name__ == '__main__':
   
   path_to_dataset = 'data_example'
 
-  path_to_video = "path/to/videos/"
+  path_to_video = "path/to/videos"
 
   videos = [{"name": "video1.m4v", "label": "label1"},
             {"name": "video2.mp4", "label": "label2"}]
 
   for video in videos:
-    extract_frame_from_video(path_to_video + video['name'], 5, path_to_save)
+    extract_frame_from_video('{}/{}'.format(path_to_video, video['name']), 5, path_to_save)
